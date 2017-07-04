1bc8378bab10003f2f6d1162f10aa80e4316342a
Marooxx <omar.hamzi@lepoles.com>
2017-07-04 13:01:20 +0200

    ajout connexionphp testphp

---
+++ admin/testconnection.php
@@ -0,0 +1,19 @@
+<?php require '../connexion/connexion.php' ?>
+
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title> Test connection</title>
+	<link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+<?php
+$sql = $pdocv->query("SELECT * FROM t_utilisateurs WHERE id_utilisateur = '1' ");
+$ligne = $sql->fetch();// va chercher information 
+
+?>
+	<p> coucou ! <?php echo $ligne['prenom'].' '.$ligne['nom'];?></p>
+</body>
+</html>
